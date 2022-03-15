1bbd8035ba5f74a97c06ea0b44c1e97ce6ec4aaf
Lin, Zhanyi <zhanyi.21@intl.zju.edu.cn>
2022-03-15 19:34:52 +0800

    v1,2022/3/15

---
+++ Practical5/variables.py
@@ -0,0 +1,17 @@
+#3.1
+a=19245301
+b=4218520
+c=271
+d=b-c
+e=a-b
+print(d-e) #d-e<0, which means the rate of new cases is greater in 2021.
+
+#3.2
+X="ILOVEU"
+Y="ILOVEU"
+W=bool(X and Y)
+print(W) # If the string X and Y have the same value, W will be True.
+X="ILOVEU"
+Y="IHATEU"
+W=bool(X and Y)
+print(W) # If the string X and Y have the different value, W will be True.
