01316b757eff904476fe8c688d247a0931110017
Antongsky <101168328+Antongsky@users.noreply.github.com>
2022-04-26 21:08:50 +0800

    Add files via upload

---
+++ Practical11/alignment.py
@@ -0,0 +1,23 @@
+import re
+raw_seq1=open("DLX5_human.fa","r")
+raw_seq2=open("DLX5_mouse.fa","r")
+raw_seq3=open("RandomSeq.fa","r")
+
+seq1=re.findall(r"\n(.+)",raw_seq1.read())[0]
+seq2=re.findall(r"\n(.+)",raw_seq2.read())[0]
+seq3=re.findall(r"\n(.+)",raw_seq3.read())[0]
+
+def alignment_score(seq1,seq2):
+    score=0
+    for i in range (0,len(seq1)):
+        if seq1[i]!=seq2[i]:
+            score+=1
+    return score
+
+print(alignment_score(seq1, seq2))
+print(alignment_score(seq2, seq3))
+print(alignment_score(seq1, seq3))
+
+print((len(seq1)-alignment_score(seq1, seq2))/len(seq1))
+print((len(seq1)-alignment_score(seq2, seq3))/len(seq1))
+print((len(seq1)-alignment_score(seq1, seq3))/len(seq1))
